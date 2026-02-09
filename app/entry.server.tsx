cd91d6964a552b62a8e2bdcfca1140a6468c4246
Goncalo Fernandes <goncalofernandes@NULL.Home>
2026-02-09 12:04:52 +0000

    Fix SSR stream import for React Router

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,7 @@
 import { PassThrough } from "stream";
 import { renderToPipeableStream } from "react-dom/server";
 import { ServerRouter } from "react-router";
-import { createReadableStreamFromReadable } from "react-router";
+import { createReadableStreamFromReadable } from "@react-router/node";
 import { type EntryContext } from "react-router";
 import { isbot } from "isbot";
 import { addDocumentResponseHeaders } from "./shopify.server";
